52b3d76e16ede23f88c39e091a5f6a7394e4d3a5
boldowa <boldowa.q@gmail.com>
2018-06-20 21:16:52 +0900

    Git version feature

--- cmake/GenerateCompilerH.cmake
+++ cmake/GenerateCompilerH.cmake
@@ -15,6 +15,25 @@ endif(NOT "Windows" STREQUAL "${CMAKE_HOST_SYSTEM_NAME}")
 
 macro(GenerateCompilerH prefix OutPath)
 
+	# Get git information
+	execute_process(OUTPUT_VARIABLE GenerateCompilerH_GitBranchName
+		WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+		COMMAND git symbolic-ref --short HEAD
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
+	#message(STATUS "GitBranch: ${GenerateCompilerH_GitBranchName}")
+	execute_process(OUTPUT_VARIABLE GenerateCompilerH_GitCommits
+		WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+		COMMAND git rev-list --count ${GenerateCompilerH_GitBranchName}
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
+	#message(STATUS "GitCommits: ${GenerateCompilerH_GitCommits}")
+	execute_process(OUTPUT_VARIABLE GenerateCompilerH_GitTag
+		WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+		COMMAND git describe --tags
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
+	#message(STATUS "GitVer: ${GenerateCompilerH_GitTag}")
+	set(GenerateCompilerH_GitRevision "${GenerateCompilerH_GitBranchName} r${GenerateCompilerH_GitCommits}(${GenerateCompilerH_GitTag})")
+	#message(STATUS "GitRevision: ${GenerateCompilerH_GitRevision}")
+
 	# cc executable
 	get_filename_component(GenerateCompilerH_C_COMPILER ${CMAKE_C_COMPILER} NAME_WE)
 	get_filename_component(GenerateCompilerH_CXX_COMPILER ${CMAKE_CXX_COMPILER} NAME_WE)
